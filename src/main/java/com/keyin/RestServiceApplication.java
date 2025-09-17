1aa64980802ad55cb269587703eb8cfd2a57c0d7
Jordan <jordan.kelloway@keyin.com>
2025-09-17 15:47:07 -0230

    Add application properties, main application class, and UI designer configuration

---
+++ src/main/java/com/keyin/RestServiceApplication.java
@@ -0,0 +1,10 @@
+package com.keyin;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(RestServiceApplication.class, args);
+    }
+}
